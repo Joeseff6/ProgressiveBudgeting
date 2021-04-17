4eb99311f999047da4711ba95d7b698cd7216458
Joseph Soria <josephsoria6@gmail.com>
2021-04-17 12:55:40 -0500

    added get route for home route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+const path = require(`path`);
 const PORT = 3000;
 
 const app = express();
@@ -28,6 +28,11 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
+app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname + `/public/index.html`));
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
