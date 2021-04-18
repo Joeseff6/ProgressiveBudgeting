fa735eeb60def383e10d4df7d8a7d2fd384691d0
Joseph Soria <josephsoria6@gmail.com>
2021-04-18 10:12:45 -0500

    Added fetch function to service worker for offline installability

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,15 +12,15 @@ const DATA_CACHE_NAME = "data-cache-v1";
 self.addEventListener("install", event => {
     event.waitUntil(
         caches
-        .open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(() => {
-            console.log(`Service worker has been installed!`);
-            self.skipWaiting();
-        })
-        .catch(err => {
-            console.log(err);
-        })
+            .open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => {
+                console.log(`Service worker has been installed!`);
+                self.skipWaiting();
+            })
+            .catch(err => {
+                console.log(err);
+            })
     );
 });
 
@@ -28,22 +28,59 @@ self.addEventListener("activate", event => {
     const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
     event.waitUntil(
         caches
-        .keys()
-        .then(cacheNames => {
-            return cacheNames.filter(
-            cacheName => !currentCaches.includes(cacheName)
-            );
-        })
-        .then(cachesToDelete => {
+            .keys()
+            .then(cacheNames => {
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName)
+                );
+            })
+            .then(cachesToDelete => {
                 return Promise.all(
-                cachesToDelete.map(cacheToDelete => {
-                    return caches.delete(cacheToDelete);
-                })
-            );
-        })
-        .then(() => self.clients.claim())
-        .catch(err => {
-            console.log(err);
+                    cachesToDelete.map(cacheToDelete => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+            .catch(err => {
+                console.log(err);
+            })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (
+        event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+    if (event.request.url.includes("/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(event.request));
+            })
+        );
+        return;
+    }
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
         })
     );
 });
